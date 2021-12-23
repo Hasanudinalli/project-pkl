f79d5bcd6c9da27a176f7d6d2a75eed9e19ce7be
Hasanudinalli <hasankasep321@gmail.com>
2021-12-23 17:18:51 +0700

    materi

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = Role::create([
+            'name' => 'admin',
+            'display_name' => 'izin admin',
+        ]);
+
+        $pengguna = Role::create([
+            'name' => 'pengguna',
+            'display_name' => 'izin pengguna',
+        ]);
+
+        $kasir = Role::create([
+            'name' => 'kasir',
+            'display_name' => 'izin kasir',
+        ]);
+
+        $UserAdmin = new User();
+        $UserAdmin->name = 'hasanudin';
+        $UserAdmin->email = 'admin@gmail.com';
+        $UserAdmin->password = Hash::make('12345678');
+        $UserAdmin->save();
+        $UserAdmin->attachRole($admin);
+
+        $UserPengguna = new User();
+        $UserPengguna->name = 'hasan';
+        $UserPengguna->email = 'hasanudin@gmail.com';
+        $UserPengguna->password = Hash::make('12345678');
+        $UserPengguna->save();
+        $UserPengguna->attachRole($pengguna);
+
+        $UserKasir = new User();
+        $UserKasir->name = 'udin';
+        $UserKasir->email = 'udin@gmail.com';
+        $UserKasir->password = Hash::maKe('12345678');
+        $UserKasir->save();
+        $UserKasir->attachRole($kasir);
+
+        $UserKasirSatu = new User();
+        $UserKasirSatu->name = 'abraham';
+        $UserKasirSatu->email = 'abraham@gmail.com';
+        $UserKasirSatu->password = Hash::maKe('12345678');
+        $UserKasirSatu->save();
+        $UserKasirSatu->attachRole($kasir);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(
+    [
+        'register' => false, //menghilangkan fitur regis
+        'reset' => false, //menghilangkan fitur forgot
+    ]
+);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' =>
+    ['auth',
+        'role:admin',
+    ]], function () {
+    Route::get('/', function () {
+        return 'halaman admin';
+    });
+
+    Route::get('profile', function () {
+        return 'halaman profile admin';
+    });
+});
+
+Route::group(['prefix' => 'pengguna', 'middleware' =>
+    ['auth',
+        'role:pengguna',
+    ]], function () {
+    Route::get('/', function () {
+        return 'halaman pengguna';
+    });
+
+    Route::get('profile', function () {
+        return 'halaman profile pengguna';
+    });
+});
+
+Route::group(['prefix' => 'pembelian', 'middleware' =>
+    ['auth',
+        'role:adminkasir',
+    ]], function () {
+     Route::get('/', function () {
+         return 'halaman pembelian';
+    });
+
+    Route::get('laporan', function () {
+        return 'halaman laporan pembelian';
+    });
+});
+
+
