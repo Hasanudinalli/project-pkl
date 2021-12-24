c79e6316b7eeea5d15df9fbc162b5322b738c725
Hasanudinalli <hasankasep321@gmail.com>
2021-12-24 16:48:54 +0700

    roleakses

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,24 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title','Dashboard')
+
+@section('content_header')
+
+Dashboard
+
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+welcome to this beautiful admin panel.
 
 
-                    @role('admin')
-                    Ini Untuk admin
-                    @endrole
+@stop
 
-                    @role('pegguna')
-                    Ini Untuk Pengguna
-                    @endrole
+@section('css')
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+@stop
+
+@section('js')
+
+@stop
